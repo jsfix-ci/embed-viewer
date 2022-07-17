6814aa04d6901bcd37946f2a94027d184f3df363
jsfix.live analysis <info@coana.tech>
2022-07-17 10:51:36 +0200

    JSFIX patches

--- src/bin/viewer.ts
+++ src/bin/viewer.ts
@@ -18,11 +18,11 @@ program
   .name('viewer')
   .description('This tool is used for converting "*.xmind" file to Gitlab pages')
   .usage('-i . -o ./docs/pages -f -e node_modules,src,...')
-  .option('-t, --title <value>', 'Specify the page title', 'Embed Viewer')
+  .option('-t, --title <value>','Specify the page title').preset('Embed Viewer')
   .option('-i, --input <value>', 'The "*.xmind" source folder')
   .option('-o, --output <value>', 'The output folder where the converted pages are stored')
   .option('-p, --purge', 'Purge the target folder before output pages')
-  .option('-e, --excludes <value>', 'Specify folder that will excluded during the process of scanning', 'node_modules')
+  .option('-e, --excludes <value>','Specify folder that will excluded during the process of scanning').preset('node_modules')
   .option('-l, --logo <value>', 'Specify the logo address')
   .version(packages.version)
   .parse();
